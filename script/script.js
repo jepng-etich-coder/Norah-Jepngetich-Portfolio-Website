83228ad92a6a5593c469bde76c66cd52a4297bc7
jepng-etich-coder <jepngetichkimaiyo@gmail.com>
2025-08-20 18:53:14 -0700

    feat: update contact section
     to display static email and phone number instead of input box

--- script/script.js
+++ script/script.js
@@ -4,3 +4,14 @@ const menuIcon = document.querySelector("#fa-menu-icon");
 menuIcon.addEventListener("click", (event) => {
   navLinks.classList.toggle("active");
 });
+
+// Close menu when clicking anywhere else
+document.addEventListener("click", (event) => {
+  if (
+    navLinks.classList.contains("active") &&
+    !navLinks.contains(event.target) &&
+    event.target !== menuIcon
+  ) {
+    navLinks.classList.remove("active");
+  }
+});
